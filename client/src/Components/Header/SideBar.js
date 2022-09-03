12ea00e78b315bed8b266e886142e650e58e6c02
evan-chernicky <echernicky@gmail.com>
2022-09-03 18:23:51 -0400

    added restrictions

---
+++ client/src/Components/Header/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div style={{width: "200px"}} className="h-screen fixed bg-white">
+        test
+    </div>
+  )
+}
+
+export default SideBar
