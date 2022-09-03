50d8d271f0434adb3083b79b2b0259bf05835d40
evan-chernicky <echernicky@gmail.com>
2022-09-03 12:15:17 -0400

    home edits

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -6,9 +6,9 @@ import SignUp from '../Components/Buttons/BorderWhiteButton'
 function Home() {
   return (
     <div style={{backgroundImage: `url("${Background}")`}} className="w-full h-screen flex items-center justify-center">
-      <section className="text-white text-center max-w-3xl bg-black/50 p-10">
-        <h1 className="text-4xl pb-4">Change the world and have fun while doing it!</h1>
-        <p className="text-xl">Recycle Quest is an app that let's your organization compete side-by-side while creating positive change. Score points by recycling.</p>
+      <section className="text-white text-center max-w-3xl bg-black/90 p-10 pb-16 rounded-3xl">
+        <h1 className="text-4xl pb-4  leading-normal">Change the world and have fun while doing it!</h1>
+        <p className="text-xl leading-relaxed">Recycle Quest is an app that let's your organization compete side-by-side while creating positive change. Score points by recycling.</p>
         <ul className="flex justify-center mt-12">
           <li className="px-3">
              <LoginButton to="/login" text="login"/> 
