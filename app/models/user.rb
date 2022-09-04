95de6fb106cbf9046a8dd6c49e52f23b048adfa6
tylergubser <tylergubser2200@gmail.com>
2022-09-04 10:34:10 -0500

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    belongs_to :organization
+    belongs_to :organization, optional: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
     validates :password, presence: true, :length => {:within => 6..40}
     validates :first_name, presence: true
