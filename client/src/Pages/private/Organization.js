d9920683b8aefc1d94ed2a44da5b68ee9d76eda3
evan-chernicky <echernicky@gmail.com>
2022-09-04 09:11:05 -0400

    more organization page edits

--- client/src/Pages/private/Organization.js
+++ client/src/Pages/private/Organization.js
@@ -11,7 +11,7 @@ const communityGoalProogress = ((itemsCollected / communityGoal) * 100) + "%"
       <h1 className="text-4xl mb-10">Group Name</h1>
       <section className="px-14 py-7 bg-white shadow-lg rounded-3xl flex flex-col relative border">
           <h2 className="text-2xl mb-1">Community Goal Progress</h2>
-          <h3 className="mb-6"><span className="font-bold">{itemsCollected}</span> out of 1000 items collected</h3>
+          <h3 className="mb-6"><span className="font-bold">{itemsCollected}</span> out of {communityGoal} items collected</h3>
           <div className="h-14 border rounded-full relative overflow-hidden">
             <div style={{width: communityGoalProogress}} className="bg-light-green h-full relative flex items-center">
               <span className="absolute top-0 bottom-0 right-5 m-auto text-white font-bold h-min text-md">{communityGoalProogress}</span>
