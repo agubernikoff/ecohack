f7a4030d3d750284070b585f0338ea1f8b12ac51
evan-chernicky <echernicky@gmail.com>
2022-09-04 19:44:07 -0400

    basic scanner styling

--- client/src/Pages/private/Recycle.js
+++ client/src/Pages/private/Recycle.js
@@ -5,14 +5,16 @@ import ScanResultMessage from "../../ScanResultMessage";
 function Donate({ user }) {
   const [isScanning, setIsScanning] = useState(false);
   const [result, setResult] = useState("");
+  const btnStyling = isScanning ? "bg-black" : "bg-light-green"
 
   function finishScanning(result) {
     setIsScanning(false);
     setResult(result);
   }
   return (
-    <div>
+    <div className="h-full flex items-center justify-center flex-col-reverse">
       <button
+        className={`p-14 hover:bg-dark-green duration-300 text-white text-3xl ${btnStyling}`}
         onClick={() => {
           setIsScanning(!isScanning);
           setResult("");
@@ -21,7 +23,9 @@ function Donate({ user }) {
         {isScanning ? "Finish Scanning" : "Start Scanning"}
       </button>
       {isScanning ? (
-        <Scanner finishScanning={finishScanning} user={user} />
+          <div className="max-w-lg mb-14">
+          <Scanner finishScanning={finishScanning} user={user} />
+          </div>
       ) : null}
       {result ? <ScanResultMessage result={result} /> : null}
     </div>
--- client/src/ScanResultMessage.js
+++ client/src/ScanResultMessage.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 function ScanResultMessage({ result }) {
   let navigate = useNavigate();
-  return <div>{result}</div>;
+  return <div className="text-3xl mb-10 font-bold">{result}</div>;
 }
 
 export default ScanResultMessage;
