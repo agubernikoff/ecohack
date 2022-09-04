f50e90ee7ebdd1ca51cdf8c7043976127ec49de4
evan-chernicky <echernicky@gmail.com>
2022-09-04 16:28:07 -0400

    eaderboard logic

--- client/src/Pages/private/Organization.js
+++ client/src/Pages/private/Organization.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Leaderboard from '../../Components/Leaderboard'
 
 function Organization() {
 
 const itemsCollected = 546
 const communityGoal = 1000
 const communityGoalProogress = Math.ceil((itemsCollected / communityGoal) * 100) + "%"
+
+const communityUsers = [
+  {
+    first_name: "John",
+    last_name: "Doe",
+    points: 123
+  },
+  {
+    first_name: "Evan",
+    last_name: "chernicky",
+    points: 500
+  },
+]
 
   return (
     <div className="m-16">
@@ -13,19 +27,12 @@ const communityGoalProogress = Math.ceil((itemsCollected / communityGoal) * 100)
           <h2 className="text-2xl mb-1">Community Goal Progress</h2>
           <h3 className="mb-6"><span className="font-bold">{itemsCollected}</span> out of {communityGoal} items collected</h3>
           <div className="h-14 border rounded-full relative overflow-hidden">
-            <div style={{width: communityGoalProogress}} className="bg-light-green h-full relative flex items-center">
+            <div style={{width: communityGoalProogress}} className="bg-gradient-to-r from-dark-green to-light-green h-full relative flex items-center">
               <span className="absolute top-0 bottom-0 right-5 m-auto text-white font-bold h-min text-md">{communityGoalProogress}</span>
             </div>
           </div>
       </section>
-      <section className="mt-16 px-14 py-7 bg-white shadow-lg rounded-3xl flex flex-col relative border">
-          <h2 className="text-2xl mb-6">Community Leaderboard</h2>
-          <div className="h-14 border rounded-full relative overflow-hidden">
-            <div style={{width: communityGoalProogress}} className="bg-light-green h-full relative flex items-center">
-              <span className="absolute top-0 bottom-0 right-5 m-auto text-white font-bold h-min text-md">{communityGoalProogress}</span>
-            </div>
-          </div>
-      </section>
+      <Leaderboard name="Community Leaderboard" communityUsers={communityUsers}/>
     </div>
   )
 }
